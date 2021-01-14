ca7a4a4e87803080861a15e9f2b88a0b4e9c1083
ferniemann <ferdinand.niemann@gmail.com>
2021-01-14 17:54:28 +0100

    Finale Version Color-Scheme-Changer

---
+++ uebungen/21/assets/js/main.js
@@ -0,0 +1,35 @@
+function changeColors1() {
+    document.body.style.backgroundColor = "#2f4858"
+    document.getElementById('headline').style.color = "#f6ae2d"
+    document.getElementById('ball1').style.borderColor = "white"
+    document.getElementById('ball2').style.borderColor = "transparent"
+    document.getElementById('ball3').style.borderColor = "transparent"
+    document.getElementById('ball4').style.borderColor = "transparent"
+}
+
+function changeColors2() {
+    document.body.style.backgroundColor = "#c5d86d"
+    document.getElementById('headline').style.color = "#710627"
+    document.getElementById('ball1').style.borderColor = "transparent"
+    document.getElementById('ball2').style.borderColor = "white"
+    document.getElementById('ball3').style.borderColor = "transparent"
+    document.getElementById('ball4').style.borderColor = "transparent"
+}
+
+function changeColors3() {
+    document.body.style.backgroundColor = "#defffc"
+    document.getElementById('headline').style.color = "#86626e"
+    document.getElementById('ball1').style.borderColor = "transparent"
+    document.getElementById('ball2').style.borderColor = "transparent"
+    document.getElementById('ball3').style.borderColor = "white"
+    document.getElementById('ball4').style.borderColor = "transparent"
+}
+
+function changeColors4() {
+    document.body.style.backgroundColor = "#00b9ae"
+    document.getElementById('headline').style.color = "#03312e"
+    document.getElementById('ball1').style.borderColor = "transparent"
+    document.getElementById('ball2').style.borderColor = "transparent"
+    document.getElementById('ball3').style.borderColor = "transparent"
+    document.getElementById('ball4').style.borderColor = "white"
+}
